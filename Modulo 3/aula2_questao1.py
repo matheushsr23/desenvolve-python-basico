90c38747d689cdf7c506cd6510d08604c4f408f1
Matheus Santos <152230672+matheushsr23@users.noreply.github.com>
2024-04-23 00:29:15 -0300

    Delete Modulo 3 directory

--- Modulo 3/aula2_questao1.py
+++
@@ -1,8 +0,0 @@
-idadejul = int(input('Digite a idade de Juliana: '))
-
-idadecris = int(input('Digite a idade de Cris: '))
-
-
-verify = idadejul and idadecris >=18 
-
-print (verify)
--- Modulo 3/aula2_questao2.py
+++
@@ -1,8 +0,0 @@
-idadejul = int(input('Digite a idade de Juliana: '))
-
-idadecris = int(input('Digite a idade de Cris: '))
-
-
-verify = bool(idadejul or idadecris >=18)
-
-print (verify)
